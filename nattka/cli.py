add0b8f578597a702df5859a3bec88f0efe15fc3
Michał Górny <mgorny@gentoo.org>
2020-03-29 17:13:45 +0200

    cli: Initial bug processing support
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- nattka/cli.py
+++ nattka/cli.py
@@ -6,9 +6,11 @@ import os.path
 import sys
 
 from nattka.bugzilla import (NattkaBugzilla, BugCategory,
+                             get_combined_buginfo,
                              fill_keywords_from_cc)
+from nattka.git import GitWorkTree, GitDirtyWorkTree
 from nattka.package import (find_repository, match_package_list,
-                            add_keywords)
+                            add_keywords, check_dependencies)
 
 
 log = logging.getLogger('nattka')
@@ -26,10 +28,17 @@ class NattkaCommands(object):
         log.error('Please pass --api-key or put it in ~/.bugz_token')
         raise SystemExit(1)
 
+    def get_bugzilla(self):
+        kwargs = {}
+        if self.args.bugzilla_endpoint is not None:
+            kwargs['api_url'] = self.args.bugzilla_endpoint
+
+        return NattkaBugzilla(self.get_api_key(), **kwargs)
+
     def apply(self):
         repo = find_repository(self.args.repo)
 
-        bz = NattkaBugzilla(self.get_api_key())
+        bz = self.get_bugzilla()
         for bugno, b in bz.fetch_package_list(self.args.bug).items():
             b = fill_keywords_from_cc(b, repo.known_arches)
             log.info('Bug {} ({})'.format(bugno, b.category.name))
@@ -39,7 +48,51 @@ class NattkaCommands(object):
             add_keywords(plist.items(), b.category == BugCategory.STABLEREQ)
 
     def process_bugs(self):
-        pass
+        repo = find_repository(self.args.repo)
+        git_repo = GitWorkTree(repo.location)
+        if git_repo.path != repo.location:
+            log.error('{} does not seem to be a git repository'
+                      .format(repo.location))
+            raise SystemExit(1)
+
+        bz = self.get_bugzilla()
+        bugs = bz.find_bugs(None)
+        for bno, b in bugs.items():
+            bugs[bno] = fill_keywords_from_cc(b, repo.known_arches)
+        try:
+            # start with the newest bugs
+            for bno in reversed(sorted(bugs)):
+                b = get_combined_buginfo(bugs, bno)
+                log.info('Bug {} ({})'.format(bno, b.category.name))
+                try:
+                    plist = dict(match_package_list(repo, b.atoms))
+
+                    for keywords in plist.values():
+                        # skip the bug if at least one package has undefined
+                        # keywords (i.e. neither explicitly specified nor
+                        # arches CC-ed)
+                        if not keywords:
+                            log.info('Skipping because of incomplete keywords')
+                            break
+                    else:
+                        with git_repo:
+                            add_keywords(plist.items(),
+                                         b.category == BugCategory.STABLEREQ)
+
+                            check_res, issues = check_dependencies(
+                                    repo, plist.items())
+                            if check_res:
+                                log.info('All good')
+                            else:
+                                log.warning('Issues found:')
+                                for i in issues:
+                                    # TODO
+                                    log.warning(i)
+                except Exception as e:
+                    log.error('TODO: handle exception {} {}'.format(e.__class__, e))
+        except GitDirtyWorkTree:
+            log.error('{}: working tree is dirty'.format(git_repo))
+            raise SystemExit(1)
 
 
 def main(argv):
@@ -47,6 +100,8 @@ def main(argv):
     argp.add_argument('--api-key',
                       help='Bugzilla API key (read from ~/.bugz_token '
                            'by default')
+    argp.add_argument('--bugzilla-endpoint',
+                      help='Bugzilla /rest endpoint URL')
     argp.add_argument('--repo', default='.',
                       help='Repository path (default: .)')
     subp = argp.add_subparsers(title='commands', dest='command',
@@ -58,9 +113,16 @@ def main(argv):
     appp.add_argument('bug', nargs='+', type=int,
                       help='Bug(s) to process')
 
+    prop = subp.add_parser('process-bugs',
+                           help='Process all open bugs -- apply '
+                                'keywords, test, report results')
+
     args = argp.parse_args(argv)
     cmd = NattkaCommands(args)
-    return getattr(cmd, args.command.replace('-', '_'))()
+    try:
+        return getattr(cmd, args.command.replace('-', '_'))()
+    except SystemExit as e:
+        return e.code
 
 
 if __name__ == '__main__':
